2e6b06be60f8d9e2619c022e5a6b913c43ce62d4
UsiMxi17 <usimxi711@gmail.com>
2024-11-07 15:27:24 +0200

    remove folder

--- LCA Code Work/python_programming/Python Examples/Python Examples.py
+++
@@ -1,8 +0,0 @@
-# Python Examples
-# User inputs for item and price and budget
-item_price = float(input("Enter the price of the item: "))
-budget = float(input("Enter your budget: "))
-
-# Compare budget with item price
-can_buy = budget >= item_price
-print("Can you afford the item?", can_buy)
